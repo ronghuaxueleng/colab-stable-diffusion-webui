d6959cd5158eed7312799c6ed37fcaa2f245266f
caoqiang <Xinyan1203>
2023-05-02 16:32:59 +0800

    up

---
+++ restore_env.sh
@@ -0,0 +1,7 @@
+mkdir -p /usr/local/envs/stablediffusion
+tar -xzvf /content/drive/MyDrive/conda/stablediffusion.tar.gz -C /usr/local/envs/stablediffusion
+/usr/local/bin/conda init
+source /usr/local/etc/profile.d/conda.sh activate stablediffusion
+source /usr/local/etc/profile.d/conda.sh deactivate
+conda activate stablediffusion
+python --version
