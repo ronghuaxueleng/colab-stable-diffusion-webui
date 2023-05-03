87d18392d937a5621b64bc204ffb947bf0165227
caoqiang <Xinyan1203>
2023-05-03 21:01:59 +0800

    up

--- init_env.sh
+++ init_env.sh
@@ -15,6 +15,6 @@ python --version
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple/
 pip install -q torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 torchtext==0.14.1 torchdata==0.5.1 --extra-index-url https://download.pytorch.org/whl/cu117 -U --no-cache-dir
 pip install -q xformers==0.0.16 triton==2.0.0 -U --no-cache-dir
-rm -rf /content/drive/MyDrive/conda/site-packages.tar.gz
-tar -czf /content/drive/MyDrive/conda/site-packages.tar.gz /usr/local/lib/python3.10/site-packages
-ls -lh /content/drive/MyDrive/conda/site-packages.tar.gz
+rm -rf /content/drive/MyDrive/packages/1.5.tar.gz
+tar -czf /content/drive/MyDrive/packages/1.5.tar.gz /usr/local/lib/python3.10/site-packages
+ls -lh /content/drive/MyDrive/packages/1.5.tar.gz
