e553cec5ff4a077e366137ef9b09824b2616ce39
caoqiang <caoqiang>
2024-01-18 15:58:51 +0800

    up

---
+++ statistics.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from peewee import *
+
+db = SqliteDatabase('statistics.db')
+
+
+# 文章表
+class Models(Model):
+    _id = PrimaryKeyField
+    id = CharField(unique=True)  # 模型id
+    name = CharField()  # 模型名称
+    runCount = IntegerField(default=0, null=False)
+    downloadCount = IntegerField(default=0, null=False)
+    timestamp = DateTimeField(null=True, default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+def delete_table():
+    return Models.delete().execute()
+
+
+def init_table():
+    db.connect()
+    db.create_tables([Models])
+
+
+if __name__ == '__main__':
+    init_table()
