feba41e58701eaebbd8edf143173ba21a769c9b3
caoqiang <caoqiang>
2024-01-18 16:07:25 +0800

    up

--- resultUtils.py
+++ resultUtils.py
@@ -1,18 +1,14 @@
 import json
 
+from statistics import Models
+
 with open("result.json", "r") as f:
     content = json.loads(f.read())
     res = dict()
     for data in content['data']['list']:
-        id = data['id']
-        name = data['name']
-        runCount = data['runCount']
-        downloadCount = data['downloadCount']
-        one = set()
-        one.add({
-            'id': id,
-            'name': name,
-            'runCount': runCount,
-            'downloadCount': downloadCount,
-        })
-        res[id] = one
+        model = {}
+        model['id'] = data['id']
+        model['name'] = data['name']
+        model['runCount'] = data['runCount']
+        model['downloadCount'] = data['downloadCount']
+        Models.insert(model).execute()
